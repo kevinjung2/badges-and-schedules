3366020092158e0255e510cfd634245cf666aa1a
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-13 02:50:18 -0500

    commit tree a2a6b4983e412ca53385271b6cd5c693a2103cac

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_messages = []
+  array.each { |name| badge_messages << badge_maker(name) }
+  badge_messages
+end
+
+# def assign_rooms(array)
+#   room_assignments = []
+#   room = 1
+#   array.each do |name|
+#     room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+#     room += 1
+#   end
+#   room_assignments
+# end
+
+def assign_rooms(array)
+  room_assignments = []
+  array.each.with_index(1) do |name, index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+  room_assignments
+end
+
+def printer(attendees)
+  badges_and_room_assignments = batch_badge_creator(attendees) + assign_rooms(attendees)
+  badges_and_room_assignments.each { |string| puts "#{string}"}
+end
